a406c71d07480fdd188a5c11924de9ea0517cb2f
Tung Vu Duc <tungvuduc2805@gmail.com>
2020-11-24 23:45:24 +0700

    Initial Commit

---
+++ TimeAgoDisplayKata/TimeAgoDisplayKata.h
@@ -0,0 +1,18 @@
+//
+//  TimeAgoDisplayKata.h
+//  TimeAgoDisplayKata
+//
+//  Created by Tung Vu Duc on 24/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TimeAgoDisplayKata.
+FOUNDATION_EXPORT double TimeAgoDisplayKataVersionNumber;
+
+//! Project version string for TimeAgoDisplayKata.
+FOUNDATION_EXPORT const unsigned char TimeAgoDisplayKataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TimeAgoDisplayKata/PublicHeader.h>
+
+
